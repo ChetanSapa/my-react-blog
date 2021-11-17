4dccfdb98d1ee97bf85c36ee8b2fa8416b4c6731
ChetanSapa <errollgarner@yandex.ru>
2021-11-17 19:56:38 +0100

    16. useHistory, filter, search, query string

--- src/components/Users/UsersSearchForm.tsx
+++ src/components/Users/UsersSearchForm.tsx
@@ -1,21 +1,27 @@
 import {Field, Form, Formik} from "formik";
 import React from "react";
 import {FilterType} from "../../redux/usersReducer";
+import {useSelector} from "react-redux";
+import {getUsersFilter} from "../../redux/usersSelectors";
 
 const usersSearchFormValidate = (values: any) => {
     const errors = {};
     return errors;
 }
 
+type FriendFormType = 'true' | 'false' | 'null';
 type FormType = {
     term: string,
-    friend: 'true' | 'false' | 'null'
+    friend: FriendFormType
 }
 type PropsType = {
     onFilterChange: (filter: FilterType) => void
 }
 
 export const UserSearchForm: React.FC<PropsType> = React.memo((props) => {
+
+    const filter = useSelector(getUsersFilter)
+
     const submit = (values: FormType, {setSubmitting}: { setSubmitting: (isSubmitting: boolean) => void }) => {
         const filter: FilterType = {
             term: values.term,
@@ -27,7 +33,8 @@ export const UserSearchForm: React.FC<PropsType> = React.memo((props) => {
 
     return <div>
         <Formik
-            initialValues={{term: '', friend: 'null'}}
+            enableReinitialize
+            initialValues={{term: filter.term, friend: String(filter.friend) as FriendFormType}}
             validate={usersSearchFormValidate}
             onSubmit={submit}
         >
